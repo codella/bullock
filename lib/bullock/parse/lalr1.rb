21a49bcc265a3dd105599aa83ebfa209bf4f37ea
Mauro Codella <mauro.codella@gmail.com>
2015-03-21 19:16:06 +0100

    Add missing method call

--- lib/bullock/parse/lalr1.rb
+++ lib/bullock/parse/lalr1.rb
@@ -1,7 +1,7 @@
 class Bullock::Parse::LALR1
   def initialize(start: :start)
     grammar = yield production
-    item_sets_dfa = ::Bullock::Parse::ItemSetsDfa.new(grammar)
+    item_sets_dfa = ::Bullock::Parse::ItemSetsDfa.new.from(grammar)
     extended_grammar = ::Bullock::Parse::ExtendedGrammar.new(item_sets_dfa)
     first_sets = ::Bullock::Parse::FirstSets.new(extended_grammar)
     follow_sets = ::Bullock::Parse::FollowSets.new(extended_grammar)
