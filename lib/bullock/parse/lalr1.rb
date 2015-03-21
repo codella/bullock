98db81ec7104ab376c351e89d84795da321c8894
Mauro Codella <mauro.codella@gmail.com>
2015-03-21 19:14:41 +0100

    Align to current approach

--- lib/bullock/parse/lalr1.rb
+++ lib/bullock/parse/lalr1.rb
@@ -1,11 +1,10 @@
 class Bullock::Parse::LALR1
   def initialize(start: :start)
     grammar = yield production
-    item_sets = ::Bullock::Parse::ItemSets.new(grammar)
-    translation_table = ::Bullock::Parse::TranslationTable.new(item_sets)
-    extended_grammar = ::Bullock::Parse::ExtendedGrammar.new(translation_table)
-    first_sets = ::Bullock::Parse::FirstSets(extended_grammar)
-    follow_sets = ::Bullock::Parse::FollowSets(extended_grammar)
+    item_sets_dfa = ::Bullock::Parse::ItemSetsDfa.new(grammar)
+    extended_grammar = ::Bullock::Parse::ExtendedGrammar.new(item_sets_dfa)
+    first_sets = ::Bullock::Parse::FirstSets.new(extended_grammar)
+    follow_sets = ::Bullock::Parse::FollowSets.new(extended_grammar)
     @action_goto_table = ::Bullock::Parse::ActionGotoTable.new(first_sets, follow_sets)
   end
 
