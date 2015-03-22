b2055a99d7eaee578ff5060035813cc36282cde9
Mauro Codella <mauro.codella@gmail.com>
2015-03-22 21:03:31 +0100

    Move LALR1 integration spec to Bullock integration spec

--- spec/integration/bullock/parse/lalr1_spec.rb
+++
@@ -1,7 +0,0 @@
-# This is a playground to test what a possible api for LALR1 can be.
-
-parser = Bullock::Parse::LALR1.new(start: :root) do
-  symbol(:a) do
-    expands('a b .c') { |c| }
-  end
-end
