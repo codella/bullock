2983996345411c63d95e336ac49d1696f8fbee8c
Mauro Codella <mcodella@zendesk.com>
2015-03-23 09:43:44 +0100

    Fix lexer

--- lib/bullock/lex/match_first.rb
+++ lib/bullock/lex/match_first.rb
@@ -14,18 +14,21 @@ module Bullock
         tokens = []
         environment = Bullock::Lex::Environment.new
         line = column = 1
-        while not scanner.eos?
-          rules.each do |rule, meta|
-            next unless meta[:state] == environment.current_state
-            next unless match = scanner.scan(rule)
-            outcome = environment.instance_eval(match, meta[:action])
-            line += match[0].count("\n")
-            column += match[0].rpartition("\n").last.length
-            offset = input.length - scanner.rest.length
-            token = Bullock::Lex::Token.new(outcome, match, line, column, offset, context)
-            tokens.push(token)
+        until scanner.eos?
+          catch :matched do
+            rules.each do |rule, meta|
+              next unless meta[:state] == environment.current_state
+              next unless match = scanner.scan(rule)
+              outcome = environment.instance_eval(match, meta[:action])
+              line += match[0].count("\n")
+              column += match[0].rpartition("\n").last.length
+              offset = input.length - scanner.rest.length
+              token = Bullock::Lex::Token.new(outcome, match, line, column, offset, context)
+              tokens.push(token)
+              throw :matched
+            end
+            raise "None of the specified rules could match `#{scanner.rest.first(5)}...`"
           end
-          raise "None of the specified rules could match `#{scanner.rest.first(5)}...`"
         end
         tokens
       end
