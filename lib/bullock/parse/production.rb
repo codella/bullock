dcee7c3da1091f9d62216b76342e38756b492084
Mauro Codella <mauro.codella@gmail.com>
2015-03-24 21:23:27 +0100

    Production test coverage

--- lib/bullock/parse/production.rb
+++ lib/bullock/parse/production.rb
@@ -3,14 +3,27 @@ module Bullock
     class Production
       attr_reader :symbol, :expansion
 
-      def initialize(symbol, expansion)
+      def initialize(symbol, expansion_string)
         @symbol = symbol
-        @expansion = expansion
+        @expansion = create_expansion(expansion_string)
       end
 
       def ==(other_production)
         symbol == other_production.symbol &&
           expansion == other_production.expansion
+      end
+
+      private
+
+      def create_expansion(expansion_string)
+        expansion_string.split(' ').map do |symbol_string|
+          match = /(\.?)(\w+)(\??)/.match(symbol_string)
+          {
+            symbol: match[2].to_sym,
+            argument: match[1] == '.',
+            optional: match[3] == '?'
+          }
+        end
       end
     end
   end
--- spec/bullock/parse/production_spec.rb
+++ spec/bullock/parse/production_spec.rb
@@ -1,6 +1,44 @@
 require 'bullock/parse/production'
 
 describe Bullock::Parse::Production do
+  describe ".new" do
+    it "creates production with a single expansion symbol" do
+      production = Bullock::Parse::Production.new(:symbol, 'a')
+
+      expect(production.expansion.length).to eq 1
+    end
+
+    it "creates production with two expansion symbols" do
+      production = Bullock::Parse::Production.new(:symbol, 'a b')
+
+      expect(production.expansion.length).to eq 2
+    end
+
+    it "creates production that by default is not an argument" do
+      production = Bullock::Parse::Production.new(:symbol, 'a')
+
+      expect(production.expansion.first[:argument]).to be_falsey
+    end
+
+    it "creates production that by default is mandatory" do
+      production = Bullock::Parse::Production.new(:symbol, 'a')
+
+      expect(production.expansion.first[:optional]).to be_falsey
+    end
+
+    it "creates production one symbol to pass as argument" do
+      production = Bullock::Parse::Production.new(:symbol, '.a')
+
+      expect(production.expansion.first[:argument]).to be_truthy
+    end
+
+    it "creates production one symbol that is an argument" do
+      production = Bullock::Parse::Production.new(:symbol, 'a?')
+
+      expect(production.expansion.first[:optional]).to be_truthy
+    end
+  end
+
   describe "#==" do
     it "returns false when symbol is different" do
       production = Bullock::Parse::Production.new(:symbol, 'expansion')
