74f63fc476510199cf076ccfed850d53ea551b3b
Mauro Codella <mauro.codella@gmail.com>
2015-03-26 18:24:33 +0100

    Improve production spec

--- spec/bullock/parse/production_spec.rb
+++ spec/bullock/parse/production_spec.rb
@@ -1,64 +1,80 @@
 require 'bullock/parse/production'
 
 describe Bullock::Parse::Production do
-  let(:void) { Proc.new {} }
-
   describe ".new" do
     it "creates production with a single expansion symbol" do
-      production = Bullock::Parse::Production.new(:symbol, 'a', &void)
+      production = Bullock::Parse::Production.new(:symbol, 'a') {}
 
       expect(production.expansion.length).to eq 1
     end
 
     it "creates production with two expansion symbols" do
-      production = Bullock::Parse::Production.new(:symbol, 'a b', &void)
+      production = Bullock::Parse::Production.new(:symbol, 'a b') {}
 
       expect(production.expansion.length).to eq 2
     end
 
     it "creates production that by default is not an argument" do
-      production = Bullock::Parse::Production.new(:symbol, 'a', &void)
+      production = Bullock::Parse::Production.new(:symbol, 'a') {}
 
       expect(production.expansion.first[:argument]).to be_falsey
     end
 
     it "creates production that by default is mandatory" do
-      production = Bullock::Parse::Production.new(:symbol, 'a', &void)
+      production = Bullock::Parse::Production.new(:symbol, 'a') {}
 
       expect(production.expansion.first[:optional]).to be_falsey
     end
 
     it "creates production one symbol to pass as argument" do
-      production = Bullock::Parse::Production.new(:symbol, '.a', &void)
+      production = Bullock::Parse::Production.new(:symbol, '.a') {}
 
       expect(production.expansion.first[:argument]).to be_truthy
     end
 
     it "creates production one symbol that is an argument" do
-      production = Bullock::Parse::Production.new(:symbol, 'a?', &void)
+      production = Bullock::Parse::Production.new(:symbol, 'a?') {}
 
       expect(production.expansion.first[:optional]).to be_truthy
+    end
+
+    it "raises an error when right-hand symbol is not a Symbol" do
+      expect do
+        Bullock::Parse::Production.new('string', 'a?') {}
+      end.to raise_error
+    end
+
+    it "raises an error when expansion is not present" do
+      expect do
+        Bullock::Parse::Production.new(:a_symbol, '  ') {}
+      end.to raise_error
+    end
+
+    it "raises an error when action is not specified" do
+      expect do
+        Bullock::Parse::Production.new(:a_symbol, 'expansion')
+      end.to raise_error
     end
   end
 
   describe "#==" do
     it "returns false when symbol is different" do
-      production = Bullock::Parse::Production.new(:symbol, 'expansion', &void)
-      other_production = Bullock::Parse::Production.new(:another_symbol, 'expansion', &void)
+      production = Bullock::Parse::Production.new(:symbol, 'expansion') {}
+      other_production = Bullock::Parse::Production.new(:another_symbol, 'expansion') {}
 
       expect(production == other_production).to be_falsey
     end
 
     it "returns false when expansion is different" do
-      production = Bullock::Parse::Production.new(:symbol, 'expansion', &void)
-      other_production = Bullock::Parse::Production.new(:symbol, 'another_expansion', &void)
+      production = Bullock::Parse::Production.new(:symbol, 'expansion') {}
+      other_production = Bullock::Parse::Production.new(:symbol, 'another_expansion') {}
 
       expect(production == other_production).to be_falsey
     end
 
     it "returns true when symbol and expansion are == other production's" do
-      production = Bullock::Parse::Production.new(:symbol, 'expansion', &void)
-      other_production = Bullock::Parse::Production.new(:symbol, 'expansion', &void)
+      production = Bullock::Parse::Production.new(:symbol, 'expansion') {}
+      other_production = Bullock::Parse::Production.new(:symbol, 'expansion') {}
 
       expect(production == other_production).to be_truthy
     end
