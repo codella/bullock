99c758b6fba6e261a1fec006ce509a9c39b8c0a7
Mauro Codella <mauro.codella@gmail.com>
2015-03-26 18:37:41 +0100

    Improve definition spec

--- spec/bullock/parse/definition_spec.rb
+++ spec/bullock/parse/definition_spec.rb
@@ -7,54 +7,23 @@ describe Bullock::Parse::Definition do
     expect(instance.productions).to be_empty
   end
 
-  it "gives a single production when rule is called only once" do
+  it "stores a production when rule is called only once" do
     instance = Bullock::Parse::Definition.new
 
     instance.production(:a_symbol, 'expansion') {}
 
-    expect(instance.productions.length).to eq 1
+    expected_production = Bullock::Parse::Production.new(:a_symbol, 'expansion') {}
+    expect(instance.productions).to eq [expected_production]
   end
 
-  it "stores a production also when using instance_eval" do
+  it "stores a production when using symbol" do
     instance = Bullock::Parse::Definition.new
 
-    instance.instance_eval do
-      production(:a_symbol, 'expansion') {}
+    instance.symbol(:a_symbol) do
+      produces('expansion') {}
     end
 
-    expect(instance.productions.length).to eq 1
+    expected_production = Bullock::Parse::Production.new(:a_symbol, 'expansion') {}
+    expect(instance.productions).to eq [expected_production]
   end
-
-  # it "accepts an action for a given rule" do
-  #   instance = Bullock::Parse::Definition.new
-  #   action = ->{}
-  #
-  #   instance.rule(/regex/, &action)
-  #
-  #   expect(instance.rules[/regex/][:action]).to be action
-  # end
-  #
-  # it "puts rule in the :base state if no state is specified" do
-  #   instance = Bullock::Parse::Definition.new
-  #
-  #   instance.rule(/regex/, :base) {}
-  #
-  #   expect(instance.rules[/regex/][:state]).to be :base
-  # end
-  #
-  # it "accepts rule in a different state" do
-  #   instance = Bullock::Parse::Definition.new
-  #
-  #   instance.rule(/regex/, :another_state) {}
-  #
-  #   expect(instance.rules[/regex/][:state]).to be :another_state
-  # end
-  #
-  # it "tolerates rule to be called without a block" do
-  #   instance = Bullock::Parse::Definition.new
-  #
-  #   instance.rule(/regex/)
-  #
-  #   expect(instance.rules[/regex/][:action]).to be_nil
-  # end
 end
