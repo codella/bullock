469d2533371b2b21b517de5f931f4fbb5b7dd5e3
Mauro Codella <mauro.codella@gmail.com>
2015-04-12 14:52:27 +0200

    firt follow set implementation

--- lib/bullock/parse/follow_set.rb
+++ lib/bullock/parse/follow_set.rb
@@ -1,12 +1,63 @@
+require 'set'
+
 module Bullock
   module Parse
     class FollowSet
       def process(first_set, grammar)
         follow_set = {}
 
-        follow_set[grammar.start] = [::Bullock::Lex::Token::EOS]
+        follow_set[grammar.start] = Set.new([:END])
+
+        previous_follow_set = nil
+        until follow_set == previous_follow_set
+          previous_follow_set = follow_set_dup(follow_set)
+
+          grammar.productions.each do |production|
+            production.expansion.each_with_index do |step, index|
+              next unless step.non_terminal?
+
+              follow_set[step] ||= Set.new
+
+              rest = production.expansion[index+1..-1]
+              rest_first_set = first_set_for(rest, first_set)
+
+              follow_set[step] += rest_first_set.reject { |s| s == :EMPTY }
+              if rest_first_set.include? :EMPTY
+                follow_set[step] += follow_set[production.expanded]
+              end
+            end
+          end
+        end
 
         follow_set
+      end
+
+      private
+
+      def first_set_for(symbols, first_set)
+        return Set.new([:EMPTY]) if symbols.empty?
+
+        first_set_for_symbols = Set.new
+
+        symbols.each_with_index do |symbol, index|
+          first_set_for_symbols += first_set[symbol] - [:EMPTY]
+          break unless first_set[symbol].include? :EMPTY
+          if index == symbols.length - 1
+            first_set_for_symbols << :EMPTY
+          end
+        end
+
+        first_set_for_symbols
+      end
+
+      def follow_set_dup(hash)
+        copy = {}
+
+        hash.each do |symbol, set|
+          copy[symbol] = Set.new(set)
+        end
+
+        copy
       end
     end
   end
--- spec/bullock/parse/follow_set_spec.rb
+++ spec/bullock/parse/follow_set_spec.rb
@@ -1,19 +1,62 @@
 require 'bullock/parse/follow_set'
 require 'bullock/parse/extended_symbol'
+require 'bullock/parse/extended_production'
 require 'bullock/parse/symbol'
-require 'bullock/lex/token'
+require 'set'
 
 describe Bullock::Parse::FollowSet do
-  let(:_a) { Bullock::Parse::Symbol.new(:a, false, false) }
-  let(:x_a) { Bullock::Parse::ExtendedSymbol.new(0, _a, 1) }
+  let(:x_Z) do
+    symbol = Bullock::Parse::Symbol.new(:Z, false, true)
+    Bullock::Parse::ExtendedSymbol.new(0, symbol, 1)
+  end
 
-  it "puts EOS in the follow set of the start symbol" do
-    first_set = {}
+  let(:x_a) do
+    symbol = Bullock::Parse::Symbol.new(:a, false, false)
+    Bullock::Parse::ExtendedSymbol.new(0, symbol, 1)
+  end
+
+  let(:x_b) do
+    symbol = Bullock::Parse::Symbol.new(:b, false, false)
+    Bullock::Parse::ExtendedSymbol.new(0, symbol, 1)
+  end
+
+  it "puts :END in the follow set of the start symbol" do
     grammar = double(:extended_grammar)
     allow(grammar).to receive(:start) { x_a }
+    allow(grammar).to receive(:productions) { [] }
 
+    first_set = {}
     expect(Bullock::Parse::FollowSet.new.process(first_set, grammar)).to eq ({
-      x_a => [::Bullock::Lex::Token::EOS]
+      x_a => Set.new([:END])
+    })
+  end
+
+  it "puts first set of the rest in last non-teminal follow set present in expansion" do
+    production = Bullock::Parse::ExtendedProduction.new(x_a, [x_b, x_Z], Proc.new {})
+
+    grammar = double(:extended_grammar)
+    allow(grammar).to receive(:start) { x_a }
+    allow(grammar).to receive(:productions) { [production] }
+
+    first_set = { x_Z => [:Z] }
+    expect(Bullock::Parse::FollowSet.new.process(first_set, grammar)).to eq ({
+      x_a => Set.new([:END]),
+      x_b => Set.new([:Z])
+    })
+  end
+
+  it "puts follow_set of extended in last non-teminal follow set present in expansion" do
+    production_a = Bullock::Parse::ExtendedProduction.new(x_a, [x_b], Proc.new {})
+    production_b = Bullock::Parse::ExtendedProduction.new(x_b, [x_Z], Proc.new {})
+
+    grammar = double(:extended_grammar)
+    allow(grammar).to receive(:start) { x_a }
+    allow(grammar).to receive(:productions) { [production_a, production_b] }
+
+    first_set = { x_Z => [:Z] }
+    expect(Bullock::Parse::FollowSet.new.process(first_set, grammar)).to eq ({
+      x_a => Set.new([:END]),
+      x_b => Set.new([:END])
     })
   end
 end
