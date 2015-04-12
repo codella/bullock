6785b4cd74ec53925095a1ebdb90db4e09715636
Mauro Codella <mauro.codella@gmail.com>
2015-04-12 17:02:17 +0200

    Implement lalr1 parser creation

--- lib/bullock/parse/lalr1.rb
+++ lib/bullock/parse/lalr1.rb
@@ -1,16 +1,13 @@
 class Bullock::Parse::LALR1
-  attr_reader :action_goto_table
-
-  def initialize(productions)
-    # grammar = ::Bullock::Parse::Grammar.from_productions(productions)
-    # item_sets_dfa = ::Bullock::Parse::ItemSetsDfa.new.process(grammar)
-    # extended_grammar = ::Bullock::Parse::ExtendedGrammar.new(item_sets_dfa)
-    # first_sets = ::Bullock::Parse::FirstSets.new(extended_grammar)
-    # follow_sets = ::Bullock::Parse::FollowSets.new(extended_grammar)
-    # @action_goto_table = ::Bullock::Parse::ActionGotoTable.new(first_sets, follow_sets)
+  def initialize(grammar)
+    dfa = ::Bullock::Parse::ItemSetsDfa.new.process(grammar)
+    extended_grammar = ::Bullock::Parse::ExtendedGrammar.new(dfa)
+    first_sets = ::Bullock::Parse::FirstSet.new.process(extended_grammar)
+    follow_sets = ::Bullock::Parse::FollowSet.new(first_set, extended_grammar)
+    @action_goto_table = ::Bullock::Parse::ActionGotoTable.new(follow_sets, dfa)
   end
 
   def parse(tokens)
-    action_goto_table.perform(tokens)
+    @action_goto_table.perform(tokens)
   end
 end
