4e30a5604d4b9e682185a29b6ba2a0385341b43d
Mauro Codella <mauro.codella@gmail.com>
2015-04-16 21:49:29 +0200

    fix lalr1

--- lib/bullock/parse/lalr1.rb
+++ lib/bullock/parse/lalr1.rb
@@ -4,10 +4,9 @@ class Bullock::Parse::LALR1
     extended_grammar = ::Bullock::Parse::ExtendedGrammar.new(dfa)
     first_sets = ::Bullock::Parse::FirstSet.new.process(extended_grammar)
     follow_sets = ::Bullock::Parse::FollowSet.new(first_set, extended_grammar)
-    @action_goto_table = ::Bullock::Parse::ActionGotoTable.new(follow_sets, dfa)
+    @goto_table = ::Bullock::Parse::GotoTable.new(dfa.translation_table)
   end
 
   def parse(tokens)
-    @action_goto_table.perform(tokens)
   end
 end
